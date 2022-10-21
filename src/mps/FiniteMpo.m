3215e668785685288cfecaf3fcc22c9b893c6402
lkdvos <lukas.devos@ugent.be>
2022-10-21 14:39:06 +0200

    small bugfix

--- src/mps/FiniteMpo.m
+++ src/mps/FiniteMpo.m
@@ -112,7 +112,7 @@ classdef FiniteMpo
         end
         
         function mpo = slice(mpo, i, j)
-            if isempty(mpo.O), return; end
+            if isempty(mpo(1).O), return; end
             if strcmp(i, ':')
                 i = 1:size(mpo(end).O, 2);
             end
